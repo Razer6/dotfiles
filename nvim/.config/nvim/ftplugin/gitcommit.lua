6ae84fb988a38abe28977d7fc33e77fc934335af
Robert Schilling <rschilling@rivosinc.com>
2026-02-04 21:57:17 +0100

    Add git commit config to neovim
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

---
+++ nvim/.config/nvim/ftplugin/gitcommit.lua
@@ -0,0 +1,11 @@
+-- Set the color column (ruler) at 72 characters
+vim.opt_local.colorcolumn = "72"
+
+-- Enable line wrapping
+vim.opt_local.wrap = true
+
+-- Set the wrap margin/textwidth so it auto-breaks lines at 72
+vim.opt_local.textwidth = 72
+
+-- Optional: Ensure spellcheck is on for commits
+vim.opt_local.spell = true
